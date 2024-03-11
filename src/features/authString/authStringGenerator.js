55253942a81fae30d307fd886f04beff636015eb
Nikolai Bik <voowoou@gmail.com>
2024-03-11 17:22:41 +0300

    Deploy the app

--- src/features/authString/authStringGenerator.js
+++
@@ -1,8 +0,0 @@
-import md5 from 'md5';
-
-// Получаем полную дату по iso, отделяя всё, что связано со временем, и убираем дефисы между числами
-const timeStamp = new Date().toISOString().split('T')[0].replace(/-/g, '');
-const password = 'Valantis';
-const authString = md5(`${password}_${timeStamp}`);
-
-export default authString;
